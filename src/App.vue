f80ae1589385d8ffd2b76aad12ee7e19c6433d5c
yohans <emailyyohans@gmail.com>
2025-05-03 11:12:54 +0300

    vue initialized

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+        hello
+        {{name}}
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name : "demeje"
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+</style>
